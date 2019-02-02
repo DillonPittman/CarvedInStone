b9178b7268cfa911ce1222f533ed6e53da922e83
Craig Derocher <craigderocher@Craig.local>
2019-02-02 11:50:53 -0600

    message1

--- Project2/routes/htmlRoutes.js
+++ Project2/routes/htmlRoutes.js
@@ -1,8 +1,20 @@
 var db = require("../models");
 
+
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("load", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+      
+  // Load index page
+  app.get("/index", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
