a2b27aa2704c8c378dadcd17c4f9d568575bc4d3
Jeff Lawlis <jeff.lawlis@yahoo.com>
2019-02-09 10:45:11 -0600

    switching login over

--- models/user.js
+++ models/user.js
@@ -2,27 +2,18 @@
 // // var express = require("express");
 // // var connection = require("./../config");
 // // cryptr = new Cryptr("myTotalySecretKey");
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    password: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
-    }
+module.exports = function(sequelize, Sequelize) {
+  var User = sequelize.define('user', {
+	  id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+		firstname: { type: Sequelize.STRING,notEmpty: true},
+		lastname: { type: Sequelize.STRING,notEmpty: true},
+		username: {type:Sequelize.TEXT},
+		about : {type:Sequelize.TEXT},
+		email: { type:Sequelize.STRING, validate: {isEmail:true} },
+		password : {type: Sequelize.STRING,allowNull: false }, 
+		last_login: {type: Sequelize.DATE},
+        status: {type: Sequelize.ENUM('active','inactive'),
+        defaultValue:'active' }
   });
   //Associating User model with Posts and Replies
   User.associate = function(models) {
