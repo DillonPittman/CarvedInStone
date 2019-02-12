f65ede78695720dd8cf2901c7d33e3f58e018cf3
DillonPittman <pittmanwdillon@gmail.com>
2019-02-11 21:26:04 -0600

    added icon, more tweaks

--- models/user.js
+++ models/user.js
@@ -3,8 +3,6 @@ module.exports = function(sequelize, Sequelize) {
     id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
     firstname: { type: Sequelize.STRING, notEmpty: true },
     lastname: { type: Sequelize.STRING, notEmpty: true },
-    username: { type: Sequelize.TEXT },
-    about: { type: Sequelize.TEXT },
     email: { type: Sequelize.STRING, validate: { isEmail: true } },
     password: { type: Sequelize.STRING, allowNull: false },
     last_login: { type: Sequelize.DATE },
@@ -13,6 +11,5 @@ module.exports = function(sequelize, Sequelize) {
       defaultValue: "active"
     }
   });
-
   return User;
 };
